dabf76110511156dde835a5697c67ca9a92dc15b
Andrey Tarantsov <andrey@tarantsov.com>
2017-04-09 19:52:30 +0700

    🌵 Add test for msg types

---
+++ mtproto/msg_test.go
@@ -0,0 +1,25 @@
+package mtproto
+
+import (
+	"testing"
+
+	"github.com/andreyvit/telegramapi/tl"
+)
+
+func TestMsgType(t *testing.T) {
+	tests := []struct {
+		input    tl.Object
+		expected MsgType
+	}{
+		{&TLReqPQ{}, KeyExMsg},
+		{&TLNearestDc{}, ContentMsg},
+	}
+	for _, tt := range tests {
+		actual := MsgFromObj(tt.input).Type
+		if actual != tt.expected {
+			t.Errorf("MsgFromObj(%v) == %v, expected %v", tt.input, actual, tt.expected)
+		} else {
+			t.Logf("MsgFromObj(%v) == %v", tt.input, actual)
+		}
+	}
+}
